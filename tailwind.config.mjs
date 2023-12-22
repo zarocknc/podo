bc19a380289662c95ce6239863d5cb63bfbf21b6
Joan <114535140+ProgramandoconFocbry@users.noreply.github.com>
2023-12-22 14:40:36 -0500

    animacion de texto en el video e implementacion del slider en pag. inicio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -21,7 +21,30 @@ export default {
 		]
 	},
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				typing: {
+				  "0%": {
+					width: "0%",
+					visibility: "hidden"
+				  },
+				  "100%": {
+					width: "100%"
+				  }  
+				},
+				blink: {
+				  "50%": {
+					borderColor: "transparent"
+				  },
+				  "100%": {
+					borderColor: "white"
+				  }  
+				}
+			  },
+			  animation: {
+				typing: "typing 4s  infinite alternate, blink .7s infinite"
+			  }
+		},
 	},
 	plugins: [require("daisyui")],
 	darkMode: "class",
